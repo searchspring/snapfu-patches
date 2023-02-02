d5d1008b0bb2b9cfbb89e3bf934bdbee6041725e
kevin <kevin@searchspring.com>
2023-02-02 00:02:17 -0700

    adding webpack style block classname to 0.4.0 and splitting 0.4.0 - moving devDep changes to 0.5.0

---
+++ preact/0.4.0/webpack.common.js
@@ -0,0 +1,74 @@
+const webpack = require('webpack');
+const childProcess = require('child_process');
+
+// determine branch name for branch override usage
+const branchName = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
+// class name for for branch override usage
+const styleClass = 'ss-snap-bundle-styles';
+
+module.exports = {
+	stats: {
+		modulesSort: 'size',
+		modulesSpace: 70,
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			BRANCHNAME: `"${branchName}"`,
+		}),
+		// to disable code splitting, include the following:
+		// new webpack.optimize.LimitChunkCountPlugin({
+		// 	maxChunks: 1,
+		// }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(css|scss)$/,
+				exclude: /\.module\.(css|scss)$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							attributes: { class: styleClass },
+						},
+					},
+					'css-loader',
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.module\.(css|scss)$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							attributes: { class: styleClass },
+						},
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: {
+								localIdentName: '[local]--[hash:base64:5]',
+							},
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|svg)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			react: 'preact/compat',
+			'react-dom/test-utils': 'preact/test-utils',
+			'react-dom': 'preact/compat',
+		},
+	},
+};
