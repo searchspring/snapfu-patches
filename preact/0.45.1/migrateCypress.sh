5ff5b1440876bbcaa47240ad8111d5bed5b291c0
Chris <chris.frazier@searchspring.com>
2023-06-22 15:38:24 -0700

    upgrading to cypress v12.15.0 Co-authored-by: kevin <korgon@users.noreply.github.com>

---
+++ preact/0.45.1/migrateCypress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# create new spec file directory
+mkdir -p ./tests/cypress/e2e
+
+# if integration directory exists, and is not empty
+if [[ -d ./tests/cypress/integration ]]; then
+    if [[ -n `ls ./tests/cypress/integration` ]]; then
+        cp ./tests/cypress/integration/* ./tests/cypress/e2e
+    fi
+
+    rm -rf ./tests/cypress/integration
+fi
+
+# rename spec files if they exist
+if [[ -n `ls ./tests/cypress/e2e/*.spec.js` ]]; then 
+    for file in ./tests/cypress/e2e/*.spec.js; do
+        mv -- "$file" "${file%.spec.js}.cy.js"
+    done
+fi
+
+mv ./tests/cypress/support/index.js ./tests/cypress/support/e2e.js
+
+rm ./tests/cypress.json
+
+cp ./patch/tests/cypress.config.js ./tests/cypress.config.js
+cp ./patch/tests/cypress/support/commands.js ./tests/cypress/support/commands.js
