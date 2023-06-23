8cc420827f82bde3d2083dc67f44c12da502f9f7
kevin <kevin@searchspring.com>
2023-06-23 09:55:28 -0600

    changing migration script for compatibility in github action runner

--- preact/0.45.1/migrateCypress.sh
+++ preact/0.45.1/migrateCypress.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# bash script for moving from Cypress 8.5.0 -> 12.15.0
 
 # create new spec file directory
 mkdir -p ./tests/cypress/e2e
 
 # if integration directory exists, and is not empty
-if [[ -d ./tests/cypress/integration ]]; then
-    if [[ -n `ls ./tests/cypress/integration` ]]; then
+if [ -d ./tests/cypress/integration ]; then
+    if [ -n "ls ./tests/cypress/integration" ]; then
         cp ./tests/cypress/integration/* ./tests/cypress/e2e
     fi
 
@@ -13,7 +14,7 @@ if [[ -d ./tests/cypress/integration ]]; then
 fi
 
 # rename spec files if they exist
-if [[ -n `ls ./tests/cypress/e2e/*.spec.js` ]]; then 
+if [ -n "ls ./tests/cypress/e2e/*.spec.js" ]; then 
     for file in ./tests/cypress/e2e/*.spec.js; do
         mv -- "$file" "${file%.spec.js}.cy.js"
     done
