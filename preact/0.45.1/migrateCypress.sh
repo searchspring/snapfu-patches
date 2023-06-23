4a83672a4fa3d3891eed9b88eb2c5ecfdf00a0d8
kevin <kevin@searchspring.com>
2023-06-23 10:41:22 -0600

    modifying migration script to use only single square brackets and adding logging and logic

--- preact/0.45.1/migrateCypress.sh
+++ preact/0.45.1/migrateCypress.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 # bash script for moving from Cypress 8.5.0 -> 12.15.0
 
-# create new spec file directory
-mkdir -p ./tests/cypress/e2e
+# check if we need to migrate
+if [ -f "./tests/cypress.json" ]; then
+    # create new spec file directory
+    mkdir -p ./tests/cypress/e2e
 
-# if integration directory exists, and is not empty
-if [ -d ./tests/cypress/integration ]; then
-    if [ -n "ls ./tests/cypress/integration" ]; then
-        cp ./tests/cypress/integration/* ./tests/cypress/e2e
+    # if integration directory exists, and is not empty
+    if [ -d "./tests/cypress/integration" ]; then
+        if [ -n "$(ls ./tests/cypress/integration)" ]; then
+            echo "moving spec files to ./tests/cypress/e2e"
+            cp ./tests/cypress/integration/* ./tests/cypress/e2e
+        else
+            echo "no spec files found to copy"
+        fi
+
+        rm -rf ./tests/cypress/integration
+
+        # rename spec files if they exist
+        if [ -n "$(ls ./tests/cypress/e2e/*.spec.js)" ]; then 
+            for file in ./tests/cypress/e2e/*.spec.js; do
+                echo "renaming spec file: $file -> ${file%.spec.js}.cy.js"
+                mv -- "$file" "${file%.spec.js}.cy.js"
+            done
+        else
+            echo "no spec files found to rename"
+        fi
     fi
 
-    rm -rf ./tests/cypress/integration
+    mv ./tests/cypress/support/index.js ./tests/cypress/support/e2e.js
+
+    rm ./tests/cypress.json
+
+    cp ./patch/tests/cypress.config.js ./tests/cypress.config.js
+    cp ./patch/tests/cypress/support/commands.js ./tests/cypress/support/commands.js
 fi
-
-# rename spec files if they exist
-if [ -n "ls ./tests/cypress/e2e/*.spec.js" ]; then 
-    for file in ./tests/cypress/e2e/*.spec.js; do
-        mv -- "$file" "${file%.spec.js}.cy.js"
-    done
-fi
-
-mv ./tests/cypress/support/index.js ./tests/cypress/support/e2e.js
-
-rm ./tests/cypress.json
-
-cp ./patch/tests/cypress.config.js ./tests/cypress.config.js
-cp ./patch/tests/cypress/support/commands.js ./tests/cypress/support/commands.js
