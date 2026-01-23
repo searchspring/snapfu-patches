d972a181b0e48b9959c9ff2f34f6f7aa48318c1b
kevin <kevin@searchspring.com>
2026-01-23 13:03:26 -0700

    moving to using bannerPlugin for adding managed flag - cleaning up webpack configs - changing logging to errors only

--- preact/0.72.1/src/webpack.dev.js
+++ preact/0.72.1/src/webpack.dev.js
@@ -32,39 +32,11 @@ const universal = merge(common, {
 		],
 	},
 	devtool: 'source-map',
-	devServer: {
-		server: 'https',
-		port: 3333,
-		hot: true,
-		allowedHosts: 'all',
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-		},
-		static: {
-			directory: path.join(__dirname, 'public'),
-			publicPath: ['/'],
-			watch: true,
-		},
-		devMiddleware: {
-			publicPath: '/',
-		},
-		client: {
-			overlay: {
-				errors: true,
-				warnings: false,
-				runtimeErrors: false,
-			},
-			webSocketURL: {
-				hostname: 'localhost',
-			},
-		},
-	},
-	devtool: 'source-map',
 });
 
 const modern = merge(common, {
 	mode: 'development',
-	entry: './src/modern.js',
+	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		chunkFilename: 'bundle.chunk.[fullhash:8].[id].js',
@@ -90,6 +62,34 @@ const modern = merge(common, {
 				},
 			},
 		],
+	},
+	devServer: {
+		server: 'https',
+		port: 3333,
+		hot: true,
+		allowedHosts: 'all',
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		static: {
+			directory: path.join(__dirname, 'public'),
+			publicPath: ['/'],
+			watch: true,
+		},
+		devMiddleware: {
+			publicPath: '/',
+		},
+		client: {
+			logging: 'error',
+			overlay: {
+				errors: true,
+				warnings: false,
+				runtimeErrors: false,
+			},
+			webSocketURL: {
+				hostname: 'localhost',
+			},
+		},
 	},
 	devtool: 'source-map',
 });
